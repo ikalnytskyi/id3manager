c6307850de56d5bad4f7ba398cf810569bd1b3e7
Roman Podoliaka <roman.podoliaka@gmail.com>
2024-12-27 12:36:37 +0000

    Add support for the APIC frame type
    Specification: https://id3.org/id3v2.3.0#Attached_picture
    
    ID3v2 allows storing arbitrary URLs to remote locations and directly
    embedding image data in MP3 files. The suggested text interface for
    setting the value of APIC frames is as follows:
    
    * Remote URLs (the image data will **not** be embedded)
    
    ```text
    APIC = http://path.to/some/image.png COVER_FRONT
    ```
    
    ```toml
    [[APIC]]
    url = "http://path.to/some/image.png"
    picture_type = "COVER_FRONT"
    ```
    
    * Local file URLs (the image data **will** be embedded)
    
    ```text
    APIC = file:///path/to/some/image.png COVER_FRONT
    ```
    
    ```toml
    [[APIC]]
    url = "file:///path/to/some/image.png"
    picture_type = "COVER_FRONT"
    ```
    
    * Base64 encoded image payload (the image data **will** be embedded)
    
    ```text
    APIC = data:image/jpeg;base64,ZHNhZmFzZmFkcw== COVER_FRONT
    ```
    
    ```toml
    [[APIC]]
    data = ZHNhZmFzZmFkcw==
    mime_type = "image/jpeg"
    picture_type = "COVER_FRONT"
    ```
    
    To display the data, we will:
    
    * Display remote URLs as is
    
    ```text
    APIC = http://path.to/some/image.png COVER_FRONT
    ```
    
    ```toml
    [[APIC]]
    url = "http://path.to/some/image.png"
    picture_type = "COVER_FRONT"
    mime_type = "-->"
    ```
    
    * Display embedded data as a Base64 encoded value
    
    ```text
    APIC = data:image/jpeg;base64,ZHNhZmFzZmFkcw== COVER_FRONT
    ```
    
    ```toml
    [[APIC]]
    data = "ZHNhZmFzZmFkcw=="
    picture_type = "COVER_FRONT"
    mime_type = "image/jpeg"
    ```

--- src/id3manager/formats/text.py
+++ src/id3manager/formats/text.py
@@ -1,5 +1,7 @@
+import base64
 import re
 import typing as t
+import urllib.parse as urlparse
 
 import mutagen.id3 as id3
 
@@ -32,6 +34,8 @@ class TextMetadataFormatter(MetadataFormatter):
                 value = frame.text[0]
             elif isinstance(frame, id3.UrlFrame):
                 value = frame.url
+            elif isinstance(frame, id3.APIC):
+                value = unparse_apic(frame)
             else:
                 raise ValueError(f"{frame.FrameID}: unsupported frame")
             print(frame.FrameID, "=", value, file=fileobj)
@@ -59,6 +63,8 @@ def parse_text_frames(frames_txt: str) -> t.List[id3.Frame]:
             frame = frame_cls(text=[value])
         elif issubclass(frame_cls, id3.UrlFrame):
             frame = frame_cls(url=value)
+        elif issubclass(frame_cls, id3.APIC):
+            frame = parse_apic(value)
         else:
             raise ValueError(f"{frame}: unsupported frame")
         frames.append(frame)
@@ -76,3 +82,66 @@ def parse_text_chapters(chapters_txt) -> t.List[id3.Frame]:
         id3.CHAP(start_time=start_time, sub_frames=[id3.TIT2(text=text)])
         for (start_time, text) in parts
     ]
+
+
+def _parse_picture_type(value: str) -> t.Tuple[str, id3.PictureType]:
+    # Expected format: "URL[ picture_type]".
+
+    url, *params = value.split(" ", maxsplit=1)
+    type_name_or_value = params[0] if params else None
+
+    return url, utils.get_apic_picture_type(type_name_or_value)
+
+
+def _parse_data_url(path: str) -> t.Tuple[str, t.Optional[str]]:
+    # Expected format: "[mime-type][;base64],<data>".
+
+    try:
+        metadata, data = path.split(",", maxsplit=1)
+    except ValueError:
+        raise ValueError(f"Invalid data URL value: `data:{path}`")
+
+    try:
+        mime_type, encoding = metadata.split(";", maxsplit=1)
+    except ValueError:
+        mime_type = metadata or None
+        encoding = None
+
+    if encoding and encoding != "base64":
+        raise ValueError(f"Unsupported encoding: `{encoding}`")
+
+    return data, mime_type
+
+
+def parse_apic(value: str) -> id3.APIC:
+    url, picture_type = _parse_picture_type(value)
+
+    result = urlparse.urlparse(url)
+    if result.scheme == "data":
+        # Data URL (https://developer.mozilla.org/en-US/docs/Web/URI/Schemes/data).
+        # Image data is embedded in the URL as a base64 value and stored alongside
+        # the MIME type.
+        #
+        # E.g. "data:image/png;base64,SGVsbG8sIFdvcmxkIQ==" or "data:,SGVsbG8sIFdvcmxkIQ==".
+        data, mime_type = _parse_data_url(result.path)
+        return utils.create_apic_frame(
+            data=data, mime_type=mime_type, picture_type=picture_type
+        )
+    else:
+        return utils.create_apic_frame(url=result, picture_type=picture_type)
+
+
+def unparse_apic(frame: id3.APIC) -> str:
+    if frame.mime == "-->":
+        # Remote URL. Represented by its location.
+        value = frame.data.decode()
+    else:
+        # Embedded data. Represented as a "data" URL.
+        data = base64.b64encode(frame.data).decode()
+        value = f"data:{frame.mime};base64,{data}"
+
+    if frame.type:
+        value += " "
+        value += str(frame.type).split(".")[-1]
+
+    return value
